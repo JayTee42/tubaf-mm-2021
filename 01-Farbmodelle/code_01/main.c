594daf9dcce10ab9d309b784f7282ba09a8746c3
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-04-14 19:45:45 +0200

    Colored dithering

--- 01-Farbmodelle/code_01/main.c
+++ 01-Farbmodelle/code_01/main.c
@@ -1,8 +1,50 @@
 #include <assert.h>
+#include <limits.h>
 #include <stdlib.h>
 
 #include "bitmap.h"
 
+// Comment this line out to disable Floyd-Steinberg dithering.
+#define DITHER
+
+// The palette colors to select from
+bitmap_pixel_rgb_t PAL[] =
+{
+	{ .r = 0x00, .g = 0x00, .b = 0x00 }, // Black
+	{ .r = 0xFF, .g = 0x00, .b = 0x00 }, // Red
+	{ .r = 0x00, .g = 0xFF, .b = 0x00 }, // Green
+	{ .r = 0x00, .g = 0x00, .b = 0xFF }, // Blue
+	{ .r = 0xFF, .g = 0xFF, .b = 0x00 }, // Yellow
+	{ .r = 0xFF, .g = 0x00, .b = 0xFF }, // Pink
+	{ .r = 0x00, .g = 0xFF, .b = 0xFF }, // Cyan
+	{ .r = 0xFF, .g = 0xFF, .b = 0xFF }  // White
+};
+
+// Select the color from the palette that exhibits the minimum Euclidean distance to the given pixel.
+static bitmap_pixel_rgb_t select_from_pal(bitmap_pixel_rgb_t pix)
+{
+	int dist = INT_MAX;
+	bitmap_pixel_rgb_t pal;
+
+	for (int i = 0; i < (sizeof(PAL) / sizeof(*PAL)); i++)
+	{
+		bitmap_pixel_rgb_t new_pal = PAL[i];
+
+		int dr = new_pal.r - pix.r, dg = new_pal.g - pix.g, db = new_pal.b - pix.b;
+		int new_dist = (dr * dr) + (dg * dg) + (db * db);
+
+		if (new_dist < dist)
+		{
+			dist = new_dist;
+			pal = PAL[i];
+		}
+	}
+
+	return pal;
+}
+
+// Helper funcs for dithering
+#ifdef DITHER
 static void apply_quant_err_comp(bitmap_component_t* c, int qe, int fac)
 {
 	int new_c = *c + ((qe * fac) / 16);
@@ -20,27 +62,29 @@ static void apply_quant_err(bitmap_pixel_rgb_t* pixels, int width, int height, i
 		apply_quant_err_comp(&pix->b, qe[2], fac);
 	}
 }
+#endif
 
-static void floyd_steinberg(bitmap_pixel_rgb_t* pixels, int width, int height)
+static void manipulate(bitmap_pixel_rgb_t* pixels, int width, int height)
 {
 	for (int y = 0; y < height; y++)
 	{
 		for (int x = 0; x < width; x++)
 		{
-			// Get a pointer to the pixel and calculate the black / white value.
+			// Get a pointer to the pixel and select a palette color for it.
 			bitmap_pixel_rgb_t* pix = &pixels[(width * y) + x];
-			bitmap_component_t gs = ((30 * pix->r) + (59 * pix->g) + (11 * pix->b)) / 100;
-			bitmap_component_t bw = (gs > 160) * 255;
-
-			// Remember the quantization error in all components before assigning the black / white value.
-			int qe[] = { pix->r - bw, pix->g - bw, pix->b - bw };
-			pix->r = pix->g = pix->b = bw;
+			bitmap_pixel_rgb_t pal = select_from_pal(*pix);
+#ifdef DITHER
+			// Calculate the quantization error in all components.
+			int qe[] = { pix->r - pal.r, pix->g - pal.g, pix->b - pal.b };
 
 			// Distribute the error onto the neighbours.
 			apply_quant_err(pixels, width, height, x + 1, y, qe, 7);
 			apply_quant_err(pixels, width, height, x - 1, y + 1, qe, 3);
 			apply_quant_err(pixels, width, height, x, y + 1, qe, 5);
 			apply_quant_err(pixels, width, height, x + 1, y + 1, qe, 1);
+#endif
+			// Assign the new pixel value.
+			*pix = pal;
 		}
 	}
 }
@@ -63,7 +107,7 @@ int main(void)
 	assert(error == BITMAP_ERROR_SUCCESS);
 
 	// Manipulate the pixels.
-	floyd_steinberg(pixels, width, height);
+	manipulate(pixels, width, height);
 
 	// Write the pixels back.
 	bitmap_parameters_t params =
@@ -78,7 +122,7 @@ int main(void)
 	};
 
 	error = bitmapWritePixels(
-		"sails_dither.bmp",
+		"sails_mod.bmp",
 		BITMAP_BOOL_TRUE,
 		&params,
 		(bitmap_pixel_t*)pixels
