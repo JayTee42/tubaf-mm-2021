0a660a75b962d0f31790db3b5c7fb2d5e012b1eb
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-04-14 14:55:14 +0200

    WIP: Writing to modified bitmap

--- 01-Farbmodelle/code_01/main.c
+++ 01-Farbmodelle/code_01/main.c
@@ -4,6 +4,14 @@
 
 #include "bitmap.h"
 
+void manipulate(bitmap_pixel_rgb_t* pixels, int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		// TODO
+	}
+}
+
 int main(void)
 {
 	// Read the bitmap pixels.
@@ -22,6 +30,44 @@ int main(void)
 	assert(error == BITMAP_ERROR_SUCCESS);
 	printf("Bitmap dimensions: %d x %d\n", width, height);
 
+	// Manipulate the pixels.
+	manipulate(pixels, width * height);
+
+	// Write the pixels back.
+	bitmap_parameters_t params =
+	{
+		// Is this bitmap bottom-up?
+		.bottomUp = BITMAP_BOOL_TRUE,
+
+		// Width in pixels:
+		.widthPx = width,
+
+		// Height in pixels:
+		.heightPx = height,
+
+		// Which color depth is used?
+		.colorDepth = BITMAP_COLOR_DEPTH_24,
+
+		// Which compression is used?
+		.compression = BITMAP_COMPRESSION_NONE,
+
+		// Which kind of DIB header is used?
+		.dibHeaderFormat = BITMAP_DIB_HEADER_INFO,
+
+		// The color space the user provides:
+		.colorSpace = BITMAP_COLOR_SPACE_RGB
+	};
+
+	error = bitmapWritePixels(
+		"sails_mod.bmp",
+		BITMAP_BOOL_TRUE,
+		&params,
+		(bitmap_pixel_t*)pixels
+	);
+
+	assert(error == BITMAP_ERROR_SUCCESS);
+
+	// Free the memory that has been allocated by the bitmap library.
 	free(pixels);
 
 	return 0;
