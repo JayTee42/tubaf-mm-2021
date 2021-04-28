783179fd92b26619c783e1906512d607b5eb9532
bnlrnz <lorenzb@tu-freiberg.de>
2021-04-28 14:49:50 +0200

    cpuid rax 0

--- 02-SIMD/code_01/01_cpuid/cpuid.c
+++ 02-SIMD/code_01/01_cpuid/cpuid.c
@@ -15,20 +15,32 @@ int main(void) {
     /////////////////////////////////////////
 
     // Create 4 integer variables to store register outputs (RAX, RBX, RCX, RDX):
+    unsigned int rax, rbx, rcx, rdx;
 
     // CPUID Version
     // ====ASM==== Set RAX to 0, call 'cpuid', receive outputs (RAX, RBX, RCX, RDX):
-
+    __asm__ __volatile__ (
+        ".intel_syntax noprefix \n"
+        "mov rax, 0             \n"
+        "cpuid                  \n"
+        ".att_syntax prefix     \n"
+        :"=a"(rax), "=b"(rbx), "=c"(rcx), "=d"(rdx)
+    ); 
 
     // The CPUID Version is placed in RAX, print it:
+    unsigned int version = rax;
+    printf("CPUID Version %d\n", version);
 
+    // If the CPUID Version is below 1, CPUID is not supported by the CPU -> close programm here
+    if (version < 1){
+        return 0;
+    }
 
     // Manufacturer String
     // This is stored as ASCII-chars in RBX, RDX and RCX
     // Print all the 12 chars:
 
 
-    // If the CPUID Version is below 1, CPUID is not supported by the CPU -> close programm here
 
 
     // CPUID Features
