eec857719913a780ada9e8e198709f24ef6935b7
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2021-07-07 15:25:38 +0200

    Gouraud shading

---
+++ 07-OpenGL_03/code_02_flat_gouraud/include/gl_calls.h
@@ -0,0 +1,58 @@
+#ifndef GLCALLS_H
+#define GLCALLS_H
+
+// Include the GLAD loader *before* including GLFW!
+#include "glad/glad.h"
+
+// Include the GLFW library (should be the same for all OS):
+#include <GLFW/glfw3.h>
+
+typedef struct
+{
+    // Dimensions of the window:
+    int window_width;
+    int window_height;
+
+    // The current shader program:
+    GLuint shader_program;
+
+    // The uniform locations:
+    GLint angle_y_loc;
+
+    // The vertex array object (blackbox ...):
+    GLuint vao;
+
+    // The vertex buffer object (GPU mem for the vertex data):
+    GLuint vbo;
+
+    // The number of vertices to render:
+    int vertex_data_count;
+
+    // The model:
+    double last_frame_time;
+    GLfloat angle_y;
+} user_data_t;
+
+typedef struct
+{
+    GLfloat position[3];
+    GLubyte color[3];
+    GLfloat normal[3];
+} vertex_data_t;
+
+// Vertex attributes:
+#define ATTRIB_POSITION 0
+#define ATTRIB_COLOR 1
+#define ATTRIB_NORMAL 2
+
+// Generic error checks:
+void check_error(int condition, const char* error_text);
+void gl_check_error(const char* error_text);
+
+// GL functions:
+void init_gl(GLFWwindow* window);
+void update_gl(GLFWwindow* window);
+void draw_gl(GLFWwindow* window);
+void teardown_gl(GLFWwindow* window);
+
+#endif
---
+++ 07-OpenGL_03/code_02_flat_gouraud/shader/vertex.glsl
@@ -0,0 +1,57 @@
+#version 410
+
+layout(location = 0) in vec4 v_position;
+layout(location = 1) in vec4 v_color;
+layout(location = 2) in vec4 v_normal;
+
+out vec4 f_color;
+
+// Uniforms:
+uniform float angle_y;
+
+void main()
+{
+    // Useful parameters for the teapot:
+    float near = 1.0;
+    float far = 12.0;
+    float left = -1.0;
+    float right = 1.0;
+    float top = 1.0;
+    float bottom = -1.0;
+
+    // mat4() works column-wise!
+    mat4 frustum = mat4(
+        2.0 * near / (right - left),        0.0,                                0.0,                                0.0,
+        0.0,                                2.0 * near / (top - bottom),        0.0,                                0.0,
+        (right + left) / (right - left),    (top + bottom) / (top - bottom),    -(far + near) / (far - near),       -1.0,
+        0.0,                                0.0,                                -2.0 * near * far / (far - near),   0.0
+    );
+
+    mat4 rot_y = mat4(
+        cos(angle_y),       0.0,    -sin(angle_y),  0.0,
+        0.0,                1.0,    0.0,            0.0,
+        sin(angle_y),       0.0,    cos(angle_y),   0.0,
+        0.0,                0.0,    0.0,            1.0
+    );
+
+    vec4 trans = vec4(0.0, -4.0, -7.0, 1.0);
+    vec3 pos = ((rot_y * v_position) + trans).xyz; // Swizzle!
+
+    // Define the light position in world space:
+    vec3 light_pos = vec3(0.0, 0.0, 10.0);
+
+    // Calculate the vector from the vertex to the light:
+    vec3 light_vec = normalize(light_pos - pos);
+
+    // Rotate and normalize the normal vector:
+    vec3 n_normal = normalize((rot_y * v_normal).xyz);
+
+    // Calculate the dot product.
+    // It depends on angle(normal, light):
+    // 0°  => max
+    // 90° => 0
+    float light_weight = max(dot(n_normal, light_vec), 0.1);
+
+    gl_Position = frustum * vec4(pos, 1.0);
+    f_color = light_weight * v_color;
+}
